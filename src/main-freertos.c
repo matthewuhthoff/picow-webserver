5776c2c8d563f6459d0ba3e50e7ec553d40336fe
Geoff Simmons <geoff@uplex.de>
2024-06-30 21:40:30 +0200

    Simplify main-freertos.c, and remove some unused #includes.
    Now we assert that xTaskCreate() succeeds.

--- src/main-freertos.c
+++ src/main-freertos.c
@@ -9,11 +9,8 @@
 #error FreeRTOS version since V11.0.0 is required (with SMP support)
 #endif
 
-#include "pico/cyw43_arch.h"
-#include "pico/multicore.h"
-
-/* For HTTP_LOG_ERROR() */
-#include "picow_http/log.h"
+/* For PICOW_HTTP_ASSERT() */
+#include "picow_http/assertion.h"
 
 #include "tasks.h"
 
@@ -40,26 +37,22 @@ main(void)
 	main_init();
 
 	/* Create tasks for all of the initialization functions. */
-	if ((ret = xTaskCreate(initiate_temp, "temp", configMINIMAL_STACK_SIZE,
-			       NULL, TASK_PRIO, &temp_task)) != pdPASS) {
-		HTTP_LOG_ERROR("Failed to create temp task: %d", ret);
-		exit(-1);
-	}
+	ret = xTaskCreate(initiate_temp, "temp", configMINIMAL_STACK_SIZE,
+			  NULL, TASK_PRIO, &temp_task);
+	PICOW_HTTP_ASSERT(ret == pdPASS);
+
 	/*
 	 * Pass in the linkup semaphore, so that the at-time worker for
 	 * rssi updates starts after the wifi connection reaches the
 	 * linkup state.
 	 */
-	if ((ret = xTaskCreate(initiate_rssi, "rssi", configMINIMAL_STACK_SIZE,
-			       &linkup, TASK_PRIO, &rssi_task)) != pdPASS) {
-		HTTP_LOG_ERROR("Failed to create rssi task: %d", ret);
-		exit(-1);
-	}
-	if ((ret = xTaskCreate(initiate_http, "http", HTTP_STACK_SIZE, NULL,
-			       TASK_PRIO + 1, &http_task)) != pdPASS) {
-		HTTP_LOG_ERROR("Failed to create http task: %d", ret);
-		exit(-1);
-	}
+	ret = xTaskCreate(initiate_rssi, "rssi", configMINIMAL_STACK_SIZE,
+			  &linkup, TASK_PRIO, &rssi_task);
+	PICOW_HTTP_ASSERT(ret == pdPASS);
+
+	ret = xTaskCreate(initiate_http, "http", HTTP_STACK_SIZE, NULL,
+			  TASK_PRIO + 1, &http_task);
+	PICOW_HTTP_ASSERT(ret == pdPASS);
 
 	vTaskStartScheduler();
 
